eb0abcafc65a86a8b46ab7ff1bc9aa75ab7a60ba
5ma <bbxxuw@gmail.com>
2024-03-08 02:14:42 +0900

    🏷️ add types global use

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Color =
+  | `#${string}`
+  | `rgb(${number},${number},${number})`
+  | `rgba(${number},${number},${number},${number})`
+  | 'transparent';
+
+export type Image = {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+};
+
+export type HeadingTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
