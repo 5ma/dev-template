818e1b19091092d67fdb59cf925db33317d076bc
5ma <bbxxuw@gmail.com>
2024-03-08 10:34:49 +0900

    ✨ add ImageResponsive component

---
+++ src/tailwind-variables.ts
@@ -0,0 +1,13 @@
+const screens = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1440px',
+  '3xl': '1680px',
+  '4xl': '1920px',
+} as const;
+
+export const theme = {
+  screens,
+};
