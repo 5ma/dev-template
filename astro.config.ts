fc0154d3959ba19463d0f51a78aa9fd857802631
5ma <bbxxuw@gmail.com>
2024-03-28 02:34:15 +0900

    ✨ add PictureResponsive component

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from 'astro-imagetools';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({ nesting: true })],
+  integrations: [mdx(), sitemap(), tailwind({ nesting: true }), astroImageTools],
   vite: {
     define: {
       'import.meta.vitest': 'undefined',
